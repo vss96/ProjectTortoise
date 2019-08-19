e34a8c5b8b109987040f8869cd57210197950386
Vikas Shetty <vikass@sahajsoft.com>
2019-08-19 23:06:07 +0530

    Adding pull operation

--- project_tortoise/src/circular_queue/mod.rs
+++ project_tortoise/src/circular_queue/mod.rs
@@ -3,7 +3,7 @@ extern crate queues;
 
 use queues::*;
 use std::fs::{OpenOptions,File};
-use std::io::{BufReader,BufferWriter};
+use std::io::{BufReader,BufWriter};
 use std::string::String;
 
 
@@ -16,10 +16,9 @@ use std::string::String;
 
     trait QueueOperations {
         fn set_size(self : &mut Self, c_size: i32);
-        fn push( self : &mut Self, message_id : String);
+        fn push( self : &mut Self, json_message : String);
         fn pull( self : &mut Self);
         fn save_in_file(self : &mut Self,json_message: String);
-        fn read_file( self : &mut Self) -> String;
     }
 
     impl Default for CircularQueue{
@@ -37,36 +36,34 @@ use std::string::String;
             self.c_size = c_size;
         }
 
-        fn push(self : &mut Self, message_id : String) {
+        fn push(self : &mut Self, json_message : String) {
 
             if self.c_queue.size() == self.c_size {
                 saveInFile(self.c_queue.peek());
                 self.c_queue.remove();
-                self.c_queue.add(message_id);
+                self.c_queue.add(json_message);
             }
             else {
-                self.c_queue.add(message_id);
+                self.c_queue.add(json_message);
             }
         }
 
         fn pull(self : &mut Self){
-
-            let mut message : String;
-            while self.c_queue.size() < self.c_size {
-                message = read_file(self);
-                self.c_queue.add(message)
+            //Does not remove lines, will have to look into it.
+            let file = BufReader::new(self.file_pointer);
+            for line in file.lines() {
+                if self.c_queue.size() < self.c_size {
+                    self.c_queue.add(String::from_utf8(line));
+                }
+                else {
+                    break;
+                }
             }
-
         }
 
         fn save_in_file(self : &mut Self, json_message : String){
-            let  file = BufferWriter::new(self.file_pointer);
-            file.write_all(json_message.as_bytes()).expect("Unable to write into the log file");
-
-        }
-
-        fn read_file(self : &mut Self) -> String {
-            let file = BufferReader::new(self.file_pointer);
+            let  file = Buf::new(self.file_pointer);
+            file.write_all(json_message.trim().as_bytes()).expect("Unable to write into the log file");
 
         }
     }
