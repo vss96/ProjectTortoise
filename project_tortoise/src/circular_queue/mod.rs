f7df825a8d56c5728c3af2d513c8faff0e4a970a
Dantin Kakkar <dantink@sahajsoft.com>
2019-09-08 19:12:07 +0530

    Adds blocking queue

--- project_tortoise/src/circular_queue/mod.rs
+++ project_tortoise/src/circular_queue/mod.rs
@@ -1,11 +1,12 @@
 extern crate queues;
 
 use crossbeam_queue::ArrayQueue;
-use std::borrow::BorrowMut;
+use std::borrow::{BorrowMut, Borrow};
 use std::cmp::min;
 use std::fs::File;
 use std::io::{BufRead, BufReader, BufWriter, Write};
 use std::string::String;
+use std::sync::{Condvar, Mutex, Arc};
 
 pub struct CircularQueue {
     c_queue: ArrayQueue<String>,
@@ -14,10 +15,66 @@ pub struct CircularQueue {
     read_pointer: BufReader<File>,
 }
 
-pub trait QueueOperations {
+pub struct Queue {
+    name: String,
+    size: usize,
+    _canPush: Arc<(Mutex<bool>, Condvar)>,
+    _queue: ArrayQueue<Vec<u8>>
+}
+
+impl Default for Queue {
+
+    fn default() -> Self {
+        Queue {
+            name: String::from("Default"),
+            size: 100000,
+            _canPush: Arc::new((Mutex::new(true), Condvar::new())),
+            _queue: ArrayQueue::new(100000)
+        }
+    }
+}
+
+impl QueueOperations<Vec<u8>> for Queue {
+
+    fn push(&self, json_message: Vec<u8>) {
+        let isFull = self._queue.is_full();
+        let lock: &std::sync::Mutex<bool> = &self._canPush.0;
+        let condVar = &self._canPush.1;
+        let mut mutex = lock.lock().unwrap();
+        while !*mutex {
+            mutex = condVar.wait(mutex).unwrap();
+        }
+        if self.size - self._queue.len() == 1 {
+            *mutex = false;
+            condVar.notify_one();
+        }
+        self._queue.push(json_message);
+    }
+
+    fn pull(&self) -> Vec<u8> {
+        let message = self._queue.pop().unwrap_or_default();
+        let (lock, condVar) = &*self._canPush;
+        let mut mutex = lock.lock().unwrap();
+        *mutex = true;
+        condVar.notify_one();
+        message
+    }
+}
+
+impl Queue {
+
+    pub fn get_size(&self) -> usize {
+        self._queue.len()
+    }
+}
+
+pub trait Resizable {
     fn set_size(&mut self, c_size: usize);
-    fn push(&mut self, json_message: String);
-    fn pull(&mut self) -> String;
+}
+
+pub trait QueueOperations<T> {
+    fn push(&self, json_message: T);
+    fn pull(&self) -> T;
 }
 
 impl Default for CircularQueue {
@@ -31,73 +88,77 @@ impl Default for CircularQueue {
     }
 }
 
-impl QueueOperations for CircularQueue {
-    fn set_size(&mut self, c_size: usize) {
-        self.c_size = min(c_size, 100000);
-    }
-
-    fn push(&mut self, json_message: String) {
-        if self.c_queue.capacity() == self.c_size {
-            self.save_in_file(json_message);
-        } else {
-            self.c_queue.push(json_message);
-        }
-    }
-
-    fn pull(&mut self) -> String {
-        let mut message = self.c_queue.pop().unwrap_or_default();
-        self.get_file_message();
-        return String::from(message.trim_end_matches("\n"));
-    }
-}
-
-impl CircularQueue {
-    fn save_in_file(&mut self, json_message: String) {
-        let mut file = BufWriter::new(self.write_pointer.borrow_mut());
-        file.write_all((json_message + "\n").as_bytes())
-            .expect("Unable to write into the log file");
-    }
-
-    fn get_file_message(&mut self) {
-        //Does not remove lines, will have to look into it.
-        let reader = self.read_pointer.borrow_mut();
-        let mut line_string: String = String::from("");
-        reader.read_line(line_string.borrow_mut()).unwrap();
-        if !line_string.is_empty() {
-            self.c_queue.push(line_string);
-        }
-    }
-}
-
-#[test]
-fn default_test() {
-    let mut _cqueue: CircularQueue = Default::default();
-    assert_eq!(_cqueue.c_size, 100000);
-    _cqueue.set_size(100);
-    assert_eq!(_cqueue.c_size, 100);
-
-    _cqueue.push(String::from("Hello"));
-    assert_eq!(_cqueue.pull(), "Hello");
-}
-
-#[test]
-fn operation_test() {
-    let mut _cqueue = CircularQueue {
-        write_pointer: File::create("tortoise_test.log").unwrap(),
-        read_pointer: BufReader::new(File::open("tortoise_test.log").unwrap()),
-        ..Default::default()
-    };
-    _cqueue.set_size(1);
-    _cqueue.push("1".to_string());
-    _cqueue.push("2".to_string());
-    _cqueue.push("3".to_string());
-    _cqueue.push("4".to_string());
-    _cqueue.push("5".to_string());
-    assert_eq!("1", _cqueue.pull());
-    _cqueue.push("6".to_string());
-    assert_eq!("2", _cqueue.pull());
-    assert_eq!("3", _cqueue.pull());
-    assert_eq!("4", _cqueue.pull());
-    assert_eq!("5", _cqueue.pull());
-    assert_eq!("6", _cqueue.pull());
-}
+//impl QueueOperations<String> for CircularQueue {
+//
+//    fn push(&mut self, json_message: String) {
+//        if self.c_queue.capacity() == self.c_size {
+//            self.save_in_file(json_message);
+//        } else {
+//            self.c_queue.push(json_message);
+//        }
+//    }
+//
+//    fn pull(&mut self) -> String {
+//        let mut message = self.c_queue.pop().unwrap_or_default();
+//        self.get_file_message();
+//        return String::from(message.trim_end_matches("\n"));
+//    }
+//}
+//
+//impl Resizable for CircularQueue {
+//
+//    fn set_size(&mut self, c_size: usize) {
+//        self.c_size = min(c_size, 100000);
+//    }
+//}
+//
+//impl CircularQueue {
+//    fn save_in_file(&mut self, json_message: String) {
+//        let mut file = BufWriter::new(self.write_pointer.borrow_mut());
+//        file.write_all((json_message + "\n").as_bytes())
+//            .expect("Unable to write into the log file");
+//    }
+//
+//    fn get_file_message(&mut self) {
+//        //Does not remove lines, will have to look into it.
+//        let reader = self.read_pointer.borrow_mut();
+//        let mut line_string: String = String::from("");
+//        reader.read_line(line_string.borrow_mut()).unwrap();
+//        if !line_string.is_empty() {
+//            self.c_queue.push(line_string);
+//        }
+//    }
+//}
+//
+//#[test]
+//fn default_test() {
+//    let mut _cqueue: CircularQueue = Default::default();
+//    assert_eq!(_cqueue.c_size, 100000);
+//    _cqueue.set_size(100);
+//    assert_eq!(_cqueue.c_size, 100);
+//
+//    _cqueue.push(String::from("Hello"));
+//    assert_eq!(_cqueue.pull(), "Hello");
+//}
+//
+//#[test]
+//fn operation_test() {
+//    let mut _cqueue = CircularQueue {
+//        write_pointer: File::create("tortoise_test.log").unwrap(),
+//        read_pointer: BufReader::new(File::open("tortoise_test.log").unwrap()),
+//        ..Default::default()
+//    };
+//    _cqueue.set_size(1);
+//    _cqueue.push("1".to_string());
+//    _cqueue.push("2".to_string());
+//    _cqueue.push("3".to_string());
+//    _cqueue.push("4".to_string());
+//    _cqueue.push("5".to_string());
+//    assert_eq!("1", _cqueue.pull());
+//    _cqueue.push("6".to_string());
+//    assert_eq!("2", _cqueue.pull());
+//    assert_eq!("3", _cqueue.pull());
+//    assert_eq!("4", _cqueue.pull());
+//    assert_eq!("5", _cqueue.pull());
+//    assert_eq!("6", _cqueue.pull());
+//}
--- project_tortoise/src/main.rs
+++ project_tortoise/src/main.rs
@@ -1,8 +1,28 @@
 mod circular_queue;
 
-use crate::circular_queue::{CircularQueue, QueueOperations};
+use crate::circular_queue::{CircularQueue, QueueOperations, Queue};
+use std::sync::Arc;
+use std::thread;
+use std::thread::sleep;
+use std::time::Duration;
 
 fn main() {
     println!("Hello, world!");
-    let mut _cqueue: CircularQueue = Default::default();
+    let _cqueue: Arc<Queue> = Arc::new(Queue::default());
+    let queue_clone = _cqueue.clone();
+    thread::spawn(move || {
+        sleep(Duration::new(1, 0));
+        for i in 0..50000 {
+            sleep(Duration::from_millis(1));
+            let message1 = String::from_utf8(queue_clone.pull());
+            println!("pulled_message: {}", message1.unwrap());
+        }
+    });
+    for i in 0..150000 {
+        let mut string = String::from("Random_String_");
+        string.push_str(i.to_string().as_str());
+        _cqueue.push(string.into_bytes());
+    }
+    println!("Finished inserting all elements: {}", _cqueue.get_size().to_string());
+    let msg = String::from_utf8(_cqueue.pull());
 }
