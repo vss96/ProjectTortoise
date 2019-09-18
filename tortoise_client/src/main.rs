4e99fad82152c08b6662766b3fda4495300794f1
vss96 <shettyvikas209@gmail.com>
2019-09-18 18:48:40 +0530

    Add wait for pull

--- tortoise_client/src/main.rs
+++ tortoise_client/src/main.rs
@@ -12,20 +12,23 @@ fn main() {
     let mut connection = TcpStream::connect(addr).expect("Connection Refused");
 
     let mut msg_counter = 0;
-    let incoming_stream = BufReader::new(connection);
-    for line in incoming_stream.lines() {
-        let mut file =
-            File::create("Files/".to_string() + msg_counter.to_string().as_ref() + ".json")
-                .unwrap();
-        msg_counter = msg_counter + 1;
-        //       println!(" Message: {}", line.unwrap());
-        file.write_all(line.unwrap().as_bytes());
-    }
-    println!("Total Messages : {}", msg_counter);
-    let total_time = startTime.to(PreciseTime::now()).num_seconds();
-    println!(
-        "Time Taken : {}, Throughput : {}",
-        total_time,
-        msg_counter / total_time
-    );
+
+
+        let incoming_stream = BufReader::new(&connection);
+        for line in incoming_stream.lines() {
+            let mut file =
+                File::create("Files/".to_string() + msg_counter.to_string().as_ref() + ".json")
+                    .unwrap();
+            msg_counter = msg_counter + 1;
+            //       println!(" Message: {}", line.unwrap());
+            file.write_all(line.unwrap().as_bytes());
+        }
+        println!("Total Messages : {}", msg_counter);
+        let total_time = startTime.to(PreciseTime::now()).num_seconds();
+        println!(
+            "Time Taken : {}, Throughput : {}",
+            total_time,
+            msg_counter / total_time
+        );
+
 }
