f4714f5c1bd2802f0ac43015bad2e95e944a4d70
dantinkakkar <dantin.kakkar@gmail.com>
2019-09-22 20:55:01 +0530

    Added formatting changes to src/main.rs

--- project_tortoise/src/main.rs
+++ project_tortoise/src/main.rs
@@ -1,6 +1,6 @@
 use std::{fs, thread};
 use std::borrow::Borrow;
-use std::io::{BufWriter, Write};
+use std::io::Write;
 use std::net::{TcpListener, TcpStream};
 use std::sync::Arc;
 
@@ -12,14 +12,13 @@ mod circular_queue;
 
 fn write_to_connection(mut stream: TcpStream, queue: Arc<Queue>) {
     loop {
-        let msgBytes = queue.pull();
-        match msgBytes {
+        let msg_bytes = queue.pull();
+        match msg_bytes {
             Ok(msg) => {
-                stream.write_all(msg.borrow());
-                stream.write_all("\n".as_bytes());
+                stream.write_all(msg.borrow()).unwrap_or_default();
+                stream.write_all("\n".as_bytes()).unwrap_or_default();
             }
-            Err(E) => {
-            }
+            Err(e) => {}
         }
     }
 }
@@ -37,7 +36,7 @@ fn spawn_push_thread(port: String, queue: Arc<Queue>) {
                     write_to_connection(stream, server_queue);
                 });
             }
-            Err(E) => {
+            Err(e) => {
                 println!("Socket failed");
             }
         }
@@ -50,17 +49,13 @@ fn main() {
     let paths = fs::read_dir("/data/finalDataSet4Sept2019").unwrap();
 
     let spawn_queue = _cqueue.clone();
-    let joinHandleOne = thread::spawn(move || {
+    let join_handle_server = thread::spawn(move || {
         spawn_push_thread(String::from("6881"), spawn_queue.clone());
     });
-    //let spawn2_queue = _cqueue.clone();
-    //let joinHandleTwo = thread::spawn(move || {
-    //    spawn_push_thread(String::from("6882"), spawn2_queue);
-    //});
 
     let mut file_count = 0;
     let n_workers = 2;
-    let joinHandleThree = thread::spawn(move || {
+    let join_handle_file_reader_thread = thread::spawn(move || {
         let pool = ThreadPool::new(n_workers);
         for path in paths {
             let queue_clone_for_worker = queue_clone.clone();
@@ -69,14 +64,13 @@ fn main() {
                 let fname = String::from(fpath.as_ref().unwrap().path().file_stem().unwrap().to_str().unwrap());
                 let pname = String::from(path.unwrap().path().to_str().unwrap());
                 let mut json = fs::read(pname).unwrap_or_default();
-                let mut appendedFileName = (":".to_owned() + &fname).into_bytes();
-                let queue_msg = json.append(&mut appendedFileName);
+                let mut appended_file_name = (":".to_owned() + &fname).into_bytes();
+                let queue_msg = json.append(&mut appended_file_name);
                 queue_clone_for_worker.push(json);
                 file_count += 1;
             });
         }
     });
-    joinHandleThree.join().unwrap();
-    joinHandleOne.join().unwrap();
-//    joinHandleTwo.join().unwrap();
+    join_handle_file_reader_thread.join().unwrap();
+    join_handle_server.join().unwrap();
 }
